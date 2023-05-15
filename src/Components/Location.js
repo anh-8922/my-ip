e7b12918dda3f4b0bce57eb333a077ba8d24c362
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-15 21:56:16 +0100

    pin location

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -1,9 +1,21 @@
-import{MdLocationPin} from 'react-icons/md'
+import { MdLocationPin } from 'react-icons/md';
 
 const LocationPin = ({ text }) => (
-    <div className="pin">
-      
-      <p className="pin-text"><MdLocationPin className="pin-icon" />{text}</p>
-    </div>
-  )
+  <div className="pin">
+    <p
+      className="pin-text"
+      style={{
+        textAlign: 'center',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <MdLocationPin className="pin-icon" style={{ height: '100%' }} />
+      {text}
+    </p>
+  </div>
+);
+
 export default LocationPin
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,39 +1,36 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react'
-import LocationPin from './Location'
+
+import React, { useState, useEffect } from 'react';
+import GoogleMapReact from 'google-map-react';
+import LocationPin from './Location';
 import useFetch from '../CustomHooks/useFetch';
 
+export default function Map() {
+  const myMap = process.env.REACT_APP_MY_MAP;
+  const myAPI = process.env.REACT_APP_MY_API;
 
-export default function Map(){
+  const [center, setCenter] = useState({ lat: 51.4934, lng: 0.0098 });
+  const [zoom, setZoom] = useState(8);
+  const { data } = useFetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${myAPI}`);
 
-  const myMap = process.env.REACT_APP_MY_MAP
-  const myAPI = process.env.REACT_APP_MY_API
-  const defaultProps = {
-    center: {
-      lat: 51.4934,
-      lng: 0.0098
-    },
-    zoom: 8
-  };
+  useEffect(() => {
+    if (data) {
+      setCenter({ lat: data.latitude, lng: data.longitude });
+      setZoom(13); 
+    }
+  }, [data]);
 
-  const{data} = useFetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${myAPI}`)
   if (!data) {
     return <div>Loading...</div>;
   }
 
   return (
-
     <div style={{ height: '50vh', width: '50%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: myMap }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+        center={center}
+        zoom={zoom}
       >
-        <LocationPin
-          lat={data.latitude}
-          lng={data.longitude}
-          text='You are here'
-        />
+        <LocationPin lat={data.latitude} lng={data.longitude} text="You are here" />
       </GoogleMapReact>
     </div>
   );
