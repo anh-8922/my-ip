9dcc54a4550c5ec8c0bf30452f54b3012d026a2d
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-15 22:15:41 +0100

    remove components from app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import IpAddress from './Components/IpAddress';
-import CountryFlag from './Components/CountryFlag';
-import DateAndTime from './Components/Date';
-import CountryData from './Components/Country';
-import Map from './Components/Map';
+
 
 function App() {
   return (
     <div className="App">
-   <IpAddress/>
-   <CountryFlag/>
-   <DateAndTime/>
-   <CountryData/>
-   <Map/>
+  
     </div>
   );
 }
