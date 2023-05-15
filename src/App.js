b0188c5870be18f8a3e9be807434b20b69e0ad47
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-15 23:02:03 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import IpAddress from './Components/IpAddress';
+import Map from './Components/Map';
+import DateAndTime from './Components/Date';
+import CountryFlag from './Components/CountryFlag'
+import CountryData from './Components/Country';
 
 
 function App() {
   return (
     <div className="App">
-  
+      <Map/>
+      <IpAddress/>
+      <DateAndTime />
+      <CountryFlag/>
+      <CountryData/>
     </div>
   );
 }
