3944f809f658483432bfe9dabbfa7b0fcccc35c5
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-16 11:57:22 +0100

    set zoom

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -9,13 +9,14 @@ export default function Map() {
   const myAPI = process.env.REACT_APP_MY_API;
 
   const [center, setCenter] = useState({ lat: 51.4934, lng: 0.0098 });
-  const [zoom, setZoom] = useState(8);
+  const [zoom, setZoom] = useState(1);
   const { data } = useFetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${myAPI}`);
+  
 
   useEffect(() => {
     if (data) {
       setCenter({ lat: data.latitude, lng: data.longitude });
-      setZoom(10); 
+      setZoom(8); 
     }
   }, [data]);
 
