fe6eda416fa657804afd5857b14e4069d1291ec5
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-17 10:39:44 +0100

    match with demo

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -1,9 +1,19 @@
 import { MdLocationPin } from 'react-icons/md';
-import './css/styles.css';
+
 const LocationPin = ({ text }) => (
   <div className="pin">
-    <p className="pin-text">
-      <MdLocationPin className="pin-icon" style={{ height: '100%' }} />
+    <p
+      className="pin-text"
+      style={{
+        textAlign: 'center',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        transformOrigin: 'center center'
+      }}
+    >
+      <MdLocationPin className="pin-icon"  style={{ height: '100%', position:'relative'}} />
       {text}
     </p>
   </div>
